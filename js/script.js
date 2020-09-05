4cbd9b08b2a259035543137cce207dcc3e0f24e8
livprojects <62610142+livprojects@users.noreply.github.com>
2020-09-05 19:47:04 +0200

    fixed some responsive bugs

--- js/script.js
+++ js/script.js
@@ -1,30 +1,22 @@
 const app = {
     init: function () {
 
-        app.menuAppears();
+        const getButton = document.getElementById('menuButton'); 
+        getButton.addEventListener('click', app.menuAppears())
 
-        const allLinks = document.querySelector('a');
-        console.log(allLinks)
-        for (let link of allLinks) {
-            link.addEventListener('click', app.textDisappears)
-        }
+
 
 
     },
 
     menuAppears: function () {
-        const getNavId = document.getElementById('leftNavId')
-        getNavId.classList.add('animate__animated'+'animate__fadeInLeft')
+        const leftNav = document.getElementById('leftNavId')
+        leftNav.style.display = 'block';
 
 
     },
 
-    textDisappears: function () {
-        const textBlock = document.getElementById('textBodyId');
-        textBlock.classList.add('animate__bounceOutRight')
-        
 
-    }
 
 }
 
